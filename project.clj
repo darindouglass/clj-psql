8b9ddf25679a24eb5a5e4c87e188c26dc6ecfd30
Darin Douglass <ddouglass@barracuda.com>
2020-05-02 14:50:12 -0400

    Specify the new snapshot version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject douglass/clj-psql "0.1.0"
+(defproject douglass/clj-psql "0.1.1-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.10.1"]]
   :test-paths ["test"]
   :resource-paths ["resources"])
