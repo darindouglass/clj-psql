f4ed37c3b8ac85b360abb35cb9241411c993a3ef
Darin Douglass <ddouglass@barracuda.com>
2020-05-02 14:43:42 -0400

    Add clojars release information

--- project.clj
+++ project.clj
@@ -1,4 +1,11 @@
 (defproject douglass/clj-psql "0.1.0"
+  :description "A small Clojure wrapper for interacting with Postgres via psql"
+  :license "EPL 1.0"
+  :url "https://github.com/DarinDouglass/clj-psql"
   :dependencies [[org.clojure/clojure "1.10.1"]]
   :test-paths ["test"]
-  :resource-paths ["resources"])
+  :resource-paths ["resources"]
+  :deploy-repositories [["clojars" {:url "https://clojars.org/repo"
+                                    :username :env/clojars_user
+                                    :password  :env/clojars_password
+                                    :sign-releases false}]])
