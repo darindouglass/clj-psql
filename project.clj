780be66da8e22c258f23d90bc943250db9f72aa6
Darin Douglass <ddouglass@barracuda.com>
2020-05-03 13:50:48 -0400

    Bump version after release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject douglass/clj-psql "0.1.1-SNAPSHOT"
+(defproject douglass/clj-psql "0.1.2-SNAPSHOT"
   :description "A small Clojure wrapper for interacting with Postgres via psql"
   :license "EPL 1.0"
   :url "https://github.com/DarinDouglass/clj-psql"
